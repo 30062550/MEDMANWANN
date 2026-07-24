30e8cf548530915fff0e3502ed287f84811ffb22
30062550 <nchampakanan@gmail.com>
2026-07-25 00:25:48 +0700

    Create page.tsx

---
+++ src/app/(shop)/review/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewsPage() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://www.canva.com/design/DAHPvmsENPI/DF0Ztw6WTPG019bvosy2vQ/view?embed"
+        className="w-full h-full border-0"
+        allow="fullscreen"
+        allowFullScreen
+        loading="lazy"
+      />
+    </div>
+  );
+}

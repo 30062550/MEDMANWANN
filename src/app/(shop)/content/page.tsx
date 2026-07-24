a1b484a9cd09b62007a58f2c785c6ab9137e5d57
30062550 <nchampakanan@gmail.com>
2026-07-25 00:26:14 +0700

    Create page.tsx

---
+++ src/app/(shop)/content/page.tsx
@@ -0,0 +1,13 @@
+export default function ContentPage() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://canva.link/sw4vjqpk4nw874j"
+        className="w-full h-full border-0"
+        allow="fullscreen"
+        allowFullScreen
+        loading="lazy"
+      />
+    </div>
+  );
+}

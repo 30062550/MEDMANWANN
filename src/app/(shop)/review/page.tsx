8f2147e8f14aa07e49c3169ba5b3bc0086b01822
30062550 <nchampakanan@gmail.com>
2026-07-25 00:29:12 +0700

    Update page.tsx

--- src/app/(shop)/review/page.tsx
+++ src/app/(shop)/review/page.tsx
@@ -2,7 +2,7 @@ export default function ReviewsPage() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="https://www.canva.com/design/DAHPvmsENPI/DF0Ztw6WTPG019bvosy2vQ/view?embed"
+        src="https://www.canva.com/design/DAHPvmsENPI/DF0Ztw6WTPG019bvosy2vQ/watch"
         className="w-full h-full border-0"
         allow="fullscreen"
         allowFullScreen
